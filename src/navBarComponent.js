4279828daaa969fea3f5c3d751d11fa7c899b3ab
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2021-06-02 18:43:45 -0600

    Test commit

--- src/navBarComponent.js
+++ src/navBarComponent.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { withAuthenticator } from "aws-amplify-react";
 
-signOut = () => {
-    Auth.signOut()
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-}
+// signOut = () => {
+//     Auth.signOut()
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+// }
 
 const NavBar = props => {
     return(
